2102d46f2e59aac512964f9d44ab91cf20471994
Dima Merechko <mfocus@mail.ru>
2019-02-27 19:43:05 +0200

    add system components

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -4,10 +4,18 @@ import {CommonModule} from '@angular/common';
 import {SharedModule} from '../shared/shared.module';
 import {SystemRoutingModule} from './system-routing.module';
 import {SystemComponent} from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlaningPageComponent } from './planing-page/planing-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
 
 @NgModule({
   declarations:[
-    SystemComponent
+    SystemComponent,
+    BillPageComponent,
+    HistoryPageComponent,
+    PlaningPageComponent,
+    RecordsPageComponent
   ],
   imports:[
     CommonModule,
